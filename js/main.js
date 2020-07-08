2422b33f1e8d108fed0c8d8ccde07d18378bd5c7
patelchanakya <chanakya1patel@gmail.com>
2020-07-07 21:53:10 -0400

    Netflic clone practice

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// js for the faq items
+function showDiv() {
+  const contentOne = document.getElementById("list-content-1");
+  if (contentOne.style.display === "none") {
+    contentOne.style.display = "block";
+  } else {
+    contentOne.style.display = "none";
+  }
+}
+
+function showDivTwo() {
+  const contentTwo = document.getElementById("list-content-2");
+  if (contentTwo.style.display === "none") {
+    contentTwo.style.display = "block";
+  } else {
+    contentTwo.style.display = "none";
+  }
+}
+
+function showDivThree() {
+  const contentThree = document.getElementById("list-content-3");
+  if (contentThree.style.display === "none") {
+    contentThree.style.display = "block";
+  } else {
+    contentThree.style.display = "none";
+  }
+}
+
+function showDivFour() {
+  const contentFour = document.getElementById("list-content-4");
+  if (contentFour.style.display === "none") {
+    contentFour.style.display = "block";
+  } else {
+    contentFour.style.display = "none";
+  }
+}
+
+function showDivFive() {
+  const contentFour = document.getElementById("list-content-5");
+  if (contentFour.style.display === "none") {
+    contentFour.style.display = "block";
+  } else {
+    contentFour.style.display = "none";
+  }
+}
